9794639837d22742ef080ef7f5bfb3f3caa022e5
taliesin <kaleissin@gmail.com>
2011-12-29 01:11:55 +0100

    Only do the app-check once, in settings

--- tagtools/__init__.py
+++ tagtools/__init__.py
@@ -1,5 +1,3 @@
-from django.db.models.loading import cache as app_cache
-
 from tagtools.tagcloud import *
 from tagtools.settings import *
 
@@ -21,13 +19,40 @@ _tag_app_map = {
 
 def _generic_get_tagcloud_for_model(model, steps=CLOUD_STEPS, min_count=CLOUD_MIN_COUNT):
     for app in PREFERRED_APP_ORDER:
-        if app_cache.get_app(app):
-            return _tag_app_map[app](model, steps, min_count)
+        return _tag_app_map[app](model, steps, min_count)
     return ()
+
+def _taggit_set_tags_for_model(tags, model):
+    from taggit.models import Tag
+    existing_tags = set(model.tags.all())
+    current_tags = set()
+    if not tags:
+        model.tags.set()
+    else:
+        for tag in tags:
+            t_obj, _ = Tag.objects.get_or_create(name=tag)
+            current_tags.add(t_obj)
+        if existing_tags != current_tags:
+            model.tags.add(*tags)
+            remove_tags = existing_tags - current_tags
+            if remove_tags:
+                model.tags.remove(*remove_tags)
+    return model, existing_tags != current_tags
+
+def _tagging_set_tags_for_model(tags, model):
+    return model, None
+
+def _generic_set_tags_for_model(tags, model):
+    if getattr(model.tags, '__module__', False):
+        return _taggit_set_tags_for_model(tags, model)
+    return model, None
 
 if TAG_APP == 'taggit':
     get_tagcloud_for_model = _taggit_cloud
+    set_tags_for_model = _taggit_set_tags_for_model
 elif TAG_APP == 'tagging':
     get_tagcloud_for_model = _tagging_cloud
+    set_tags_for_model = _tagging_set_tags_for_model
 else:
     get_tagcloud_for_model = _generic_get_tagcloud_for_model
+    set_tags_for_model = _generic_set_tags_for_model
--- tagtools/settings.py
+++ tagtools/settings.py
@@ -1,3 +1,4 @@
+from django.db.models.loading import cache as app_cache, ImproperlyConfigured
 from django.conf import settings
 
 _DEFAULT_APP_ORDER = ('taggit', 'tagging')
@@ -9,13 +10,17 @@ TAG_APP = getattr(settings, 'TAGTOOLS_TAG_APP', None)
 CLOUD_STEPS = getattr(settings, 'TAGTOOLS_CLOUD_STEPS', _DEFAULT_CLOUD_STEPS)
 CLOUD_MIN_COUNT = getattr(settings, 'TAGTOOLS_CLOUD_MIN_COUNT', _DEFAULT_CLOUD_MIN_COUNT)
 
-if not TAG_APP in _DEFAULT_APP_ORDER:
+def _set_tag_app():
+    apps = []
+    for app in PREFERRED_APP_ORDER:
+        try:
+            app_cache.get_app(app)
+            apps.append(app)
+        except ImproperlyConfigured:
+            pass
+    return apps
+
+PREFERRED_APP_ORDER = _set_tag_app()
+
+if not TAG_APP in PREFERRED_APP_ORDER:
     TAG_APP = None
-else:
-    try:
-        if TAG_APP == 'taggit':
-            import taggit
-        if TAG_APP == 'tagging': 
-            import tagging
-    except ImportError:
-        TAG_APP = None
