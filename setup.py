6199d85b6ac690e722e5556ceeef1d822cdefdef
kaleissin <kaleissin@gmail.com>
2013-12-02 21:53:13 +0100

    Version-bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = (
 )
 
 setup(name='django-tag-tools',
-        version='0.1.1',
+        version='0.1.2',
         packages=('tagtools',),
         include_package_data=True,
         zip_safe=False,
