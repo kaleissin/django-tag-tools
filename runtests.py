468e64152db9e0a90c3f5f9c779ea2180b02d7a4
kaleissin <kaleissin@gmail.com>
2015-12-27 21:47:17 +0100

    Django 1.6, 1.7 EOL, testing for newer Djangos

--- runtests.py
+++ runtests.py
@@ -5,18 +5,15 @@ from __future__ import unicode_literals
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tagtools.tests.settings'
-sys.path.append('.')
-
+import django
 from django.conf import settings
 from django.test.utils import get_runner
 
+
 if __name__ == "__main__":
-    try: # Django 1.7+
-        from django import setup
-        setup()
-    except ImportError:
-        pass
+    sys.path.append('.')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tagtools.tests.settings'
+    django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tagtools"])
--- tagtools/tests/tests.py
+++ tagtools/tests/tests.py
@@ -2,6 +2,22 @@ from __future__ import unicode_literals
 
 import unittest
 
-from django import test as djangotest
-from django.test.utils import override_settings
+from django.test import TestCase
 
+import factory
+from faker import Factory as Faker
+
+from tagtools.tools import get_tagcloud_for_model
+from tagtools.tagcloud import BaseTagCloud
+
+from tagtools.tests.models import Headline
+
+fake = Faker.create()
+
+
+class HeadlineFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Headline
+
+    headline = factory.LazyAttribute(lambda t: fake.name())
