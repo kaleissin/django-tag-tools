87e39b73c2ca312cd9d9048e68855ba1ea53e6c4
kaleissin <kaleissin@gmail.com>
2015-12-27 22:43:44 +0100

    More tests for proper tagcloud

--- tagtools/tests/tests.py
+++ tagtools/tests/tests.py
@@ -7,6 +7,8 @@ from django.test import TestCase
 import factory
 from faker import Factory as Faker
 
+from taggit.models import Tag
+
 from tagtools.tools import get_tagcloud_for_model
 
 from tagtools.tests.models import Headline
@@ -22,8 +24,47 @@ class HeadlineFactory(factory.django.DjangoModelFactory):
     headline = factory.LazyAttribute(lambda t: fake.name())
 
 
+class TagFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Tag
+
+    name = factory.LazyAttribute(lambda t: fake.name())
+
+
 class TagCloudTestCase(TestCase):
+
+    def setUp(self):
+        self.headline1 = HeadlineFactory()
+        self.headline2 = HeadlineFactory()
+        self.headline3 = HeadlineFactory()
+        self.tag1 = TagFactory()
+        self.tag2 = TagFactory()
+        self.tag3 = TagFactory()
 
     def test_get_tagcloud_for_model_empty(self):
         tagcloud = get_tagcloud_for_model(Headline)
         self.assertEqual(tagcloud, [])
+
+    def test_get_tagcloud_for_model_one(self):
+        self.headline1.tags.add(self.tag1)
+        tagcloud = get_tagcloud_for_model(Headline)
+        self.assertEqual(len(tagcloud), 1)
+        self.assertEqual(tagcloud[0]['count'], 1)
+
+    def test_get_tagcloud_for_model_several(self):
+        self.headline1.tags.add(self.tag1)
+        self.headline2.tags.add(self.tag1)
+        self.headline3.tags.add(self.tag1)
+        self.headline1.tags.add(self.tag2)
+        self.headline2.tags.add(self.tag2)
+        self.headline1.tags.add(self.tag3)
+        tagcloud = get_tagcloud_for_model(Headline)
+        self.assertEqual(len(tagcloud), 3)
+        for item in tagcloud:
+            if item['slug'] == self.tag1.slug:
+                self.assertEqual(item['count'], 3)
+            if item['slug'] == self.tag2.slug:
+                self.assertEqual(item['count'], 2)
+            if item['slug'] == self.tag3.slug:
+                self.assertEqual(item['count'], 1)
