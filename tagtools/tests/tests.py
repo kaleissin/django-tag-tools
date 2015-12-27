098cb02fc862a6c7ec1340a79a8f5df1e80c22df
kaleissin <kaleissin@gmail.com>
2015-12-27 23:13:41 +0100

    Add tests for set_tags_for_model

--- tagtools/tests/tests.py
+++ tagtools/tests/tests.py
@@ -10,6 +10,7 @@ from faker import Factory as Faker
 from taggit.models import Tag
 
 from tagtools.tools import get_tagcloud_for_model
+from tagtools.tools import set_tags_for_model
 from tagtools.tagcloud import BaseTagCloud
 
 from tagtools.tests.models import Headline
@@ -77,3 +78,30 @@ class TagCloudTestCase(TestCase):
                 self.assertEqual(item['count'], 2)
             if item['slug'] == self.tag3.slug:
                 self.assertEqual(item['count'], 1)
+
+
+class SetTagsTestCase(TestCase):
+
+    def setUp(self):
+        self.headline = HeadlineFactory()
+        self.tags = ['tag1', 'tag2', 'tag3', 'tag4']
+        for tag in self.tags:
+            TagFactory(name=tag)
+
+    def test_set_no_tag(self):
+        obj, created = set_tags_for_model(None, self.headline)
+        self.assertFalse(created)
+
+    def test_set_one_tag(self):
+        obj, created = set_tags_for_model(['tag1'], self.headline)
+        self.assertTrue(created)
+
+    def test_set_existing_tag(self):
+        self.headline.tags.add(Tag.objects.get(name='tag1'))
+        obj, created = set_tags_for_model(['tag1'], self.headline)
+        self.assertFalse(created)
+
+    def test_remove_existing_tag(self):
+        self.headline.tags.add(Tag.objects.get(name='tag1'))
+        obj, created = set_tags_for_model(None, self.headline)
+        self.assertTrue(created)
